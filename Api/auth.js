762fb38270ef3e53f8c97608d5813fde1d0f5644
JiwooL0920 <leej229@mcmaster.ca>
2020-02-15 15:54:54 -0500

    i got something for signin login.. lets check with ejs now

--- Api/auth.js
+++ Api/auth.js
@@ -1,10 +1,13 @@
 const User = require('../DB/User')
+const bcrypt = require('bcryptjs');
+
 console.log("user",User)
 
 module.exports = {
+    // https://www.youtube.com/watch?v=QCJCglPLUgg
     signUp: async (req, res, next) => {
         console.log('UsersController.signUp() called!');
-        const { username, password } = req.value.body;
+        const userParam = { username, password } = req.value.body;
 
         //check if there is a user with the same username
         const foundUser = await User.findOne({ username });
@@ -12,18 +15,30 @@ module.exports = {
             res.status(403).json({error:"username is already in use"})
         }
 
+        user.hash = bycrypt.hashSync(userParam.password,10)
         const newUser = new User({ username, password });
         await newUser.save();
         res.json({ user:"created "});
     },
 
+    //https://jasonwatmore.com/post/2018/06/14/nodejs-mongodb-simple-api-for-authentication-registration-and-user-management
     login: async (req, res, next) => {
+        const { username, password } = req.value.body;
+        const user = await User.findOne({ username });
+        if (user && bycript.compareSync(password, user.hash)) { 
+            const { hash, ...userWithoutHash } = user.toObject();
+            const token = jwt.sign({ sub: user.id }, config.secret );
+            return {
+                ...userWithoutHash,
+                token
+            }
 
+        }
     },
 
-    secret: async (req,res,next) => {
+    // secret: async (req,res,next) => {
 
-    }
+    // }
 
 
 
